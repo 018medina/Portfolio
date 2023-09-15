9e6c68933fe00803567d83e4b7ad0df097c8f786
Medina <104513058+018medina@users.noreply.github.com>
2023-09-14 23:06:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 window.sr = ScrollReveal({ reset: true });
-sr.reveal("#Sec1", { duration: 1500 });
-sr.reveal("#Sec2", { duration: 1500 });
+sr.reveal("#SecApresentacao", { duration: 2000 });
+// sr.reveal("#SecSobreMim", { duration: 2000 });
+// sr.reveal("#SecHabilidadesExperiencias", { duration: 2000 });
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.scrollY + window.innerHeight * 0.85;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+  animeScroll();
+});
